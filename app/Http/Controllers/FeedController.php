0923b75fe9d2afbd80b4d9f2117dd9c7ec57fd44
williampmendes <30899373+williampmendes@users.noreply.github.com>
2025-09-05 14:16:03 -0300

    Update FeedController.php
    melhoria de erros

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class FeedController extends Controller
 {
     public function index()
     {
-        $posts = []; // Buscar os posts dos amigos ou seguidores
+        $user = Auth::user();
+
+        // Obter IDs dos amigos aceitos
+        $friendIds = $user->friends()->pluck('id')->toArray();
+
+        // Incluir o próprio ID do usuário para ver suas postagens no feed
+        $allRelevantUserIds = array_merge([$user->id], $friendIds);
+
+        // Buscar os posts dos amigos ou seguidores e do próprio usuário
+        $posts = Post::whereIn('user_id', $allRelevantUserIds)
+                     ->with('user') // Carrega os dados do usuário que fez a postagem
+                     ->latest() // Ordena pelas postagens mais recentes
+                     ->paginate(10); // Paginação para melhor performance
+
         return view('pages.feed', compact('posts'));
     }
 }
